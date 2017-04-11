d09528a9199c04fa3b2b5df4965b7d3e5ef6112a
Andrea Finch <aefinch@vt.edu>
2017-04-10 21:33:58 -0500

    added styling and functionality

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,5 @@
-var dinoArray = [];
-
 $.ajax('./db/dinosaurs.json').done(function(data){
-	dinoArray = data.dinosaurs;
+	var dinoArray = data.dinosaurs;
 	makeDom(dinoArray);
 }).fail(function(error){
 	console.log("You've made a huge mistake", error);
@@ -10,7 +8,10 @@ $.ajax('./db/dinosaurs.json').done(function(data){
 function makeDom(myArraytoPrint){
 	var myDomString = "";
 	for (var i=0; i<myArraytoPrint.length; i++){
-		myDomString +=`<div class="dinoCard">`;
+		if (i%3===0){
+			myDomString +=`<div class="row">`;
+		}
+		myDomString +=`<div class="dinoCard col-md-4">`;
 		myDomString +=`<header><h1>${myArraytoPrint[i].type}</h1></header>`;
 		myDomString +=`<section>`;
 		myDomString +=`<img src="${myArraytoPrint[i].img}">`;
@@ -18,6 +19,27 @@ function makeDom(myArraytoPrint){
 		myDomString +=`</section>`;
 		myDomString +=`<footer>${myArraytoPrint[i].info}</footer>`;
 		myDomString +=`</div>`;
+		if (i%3===2){
+			myDomString +=`</div>`;
+		}
 	}
 	$("#dinosaurs").html(myDomString);
 }
+
+$("#dinosaurs").on("click", ".dinoCard", function(e){
+	$(".dinoCard").removeClass("dottedBorder");
+	$(this).addClass("dottedBorder");	
+	$("#textbox").val("").focus();
+});
+	$("#textbox").keyup(mirrorText);
+	function mirrorText	(e){
+		var selectedCard = $(".dottedBorder");
+		var bioTyped = $("#textbox").val();
+		var bio = selectedCard.find("p.bio");
+		if (bioTyped !==""){
+		bio.html(bioTyped);
+		}
+		if (e.keyCode===13){
+			$("#textbox").val("");
+		}
+	}
