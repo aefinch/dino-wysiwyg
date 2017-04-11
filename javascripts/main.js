1d39ec279a047bd3413f5621dc118cb098a4fbb3
Andrea Finch <aefinch@vt.edu>
2017-04-10 19:55:01 -0500

    made dino cards

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-var myString = `Callan wins all cage matches ${2+5}`;
+var dinoArray = [];
+
+$.ajax('./db/dinosaurs.json').done(function(data){
+	dinoArray = data.dinosaurs;
+	makeDom(dinoArray);
+}).fail(function(error){
+	console.log("You've made a huge mistake", error);
+});
+
+function makeDom(myArraytoPrint){
+	var myDomString = "";
+	for (var i=0; i<myArraytoPrint.length; i++){
+		myDomString +=`<div class="dinoCard">`;
+		myDomString +=`<header><h1>${myArraytoPrint[i].type}</h1></header>`;
+		myDomString +=`<section>`;
+		myDomString +=`<img src="${myArraytoPrint[i].img}">`;
+		myDomString +=`<p class="bio">${myArraytoPrint[i].bio}</p>`;
+		myDomString +=`</section>`;
+		myDomString +=`<footer>${myArraytoPrint[i].info}</footer>`;
+		myDomString +=`</div>`;
+	}
+	$("#dinosaurs").html(myDomString);
+}
